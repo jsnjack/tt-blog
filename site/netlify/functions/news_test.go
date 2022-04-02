ca54dd6aa36d513328e7e1647dc27ce757bf69e4
Yauhen Shulitski <jsnjack@gmail.com>
2022-04-02 15:18:34 +0200

    Improve testing and debugging

--- site/netlify/functions/news_test.go
+++ site/netlify/functions/news_test.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 )
 
 func TestGenerateHTMLDoc(t *testing.T) {
 	data := generateHTMLDoc("http://feeds.nos.nl/nosnieuwsalgemeen")
-	t.Errorf(string(data))
+	err := os.WriteFile("output.html", data, 0644)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	exec.Command("xdg-open", "output.html").Start()
 }
